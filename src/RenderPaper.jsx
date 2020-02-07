a9e519e8c33540f9e41f81e0613f7e8610b1f086
Damilare ADEMESO <ademesodamilare@gmail.com>
2020-02-07 12:29:47 +0100

    added sharer

--- src/RenderPaper.jsx
+++ src/RenderPaper.jsx
@@ -2,20 +2,31 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Divider } from '@material-ui/core';
 import he from 'he';
+import Share from './Share';
 
 const styles = {
+    rel:{
+        position: 'relative',
+    },
     title: {
         cursor: 'pointer',
         padding: '20px',
         backgroundColor: '#3f51b5',
         color: 'white',
-
+    },
+    shareCont:{
+        backgroundColor: 'white',
+        borderRadius: '50%',
+        position: 'absolute',
+        right: '-7px',
+        bottom: '-7px'
     }
 }
 
 function RenderPaper(props) {
     var [content, setContent] = useState([]);
     const { url, paperName } = props;
+    const zijelaUrl = "http://news.zijela.com"
 
     function goToSite(newsUrl) {
         window.open(
@@ -43,7 +54,14 @@ function RenderPaper(props) {
         {
             content.map((news, index) => (
                 <div key={index}>
-                    <p style={styles.title} onClick={() => goToSite(news['guid']['rendered'])}>{he.decode(news['title'].rendered)}</p>
+                    <div style={styles.rel}>
+                    <p style={styles.title} onClick={() => goToSite(news['guid']['rendered'])}>
+                        {he.decode(news['title'].rendered)}
+                    </p>
+                        <div style={styles.shareCont}>
+                            <Share url={zijelaUrl} title={he.decode(news['title'].rendered)} paper={paperName}/>
+                        </div>
+                    </div>
                     <Divider />
                 </div>
             ))
