0ad77e2436e23759cf9e5d1a7b68f092d6bfeb83
Damilare ADEMESO <ademesodamilare@gmail.com>
2020-02-10 10:02:28 +0100

    fixed loader

---
+++ src/Loader.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+
+export default function CustomizedProgressBars() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <LinearProgress className={classes.margin} />
+      <LinearProgress className={classes.margin} />
+      <LinearProgress className={classes.margin} />
+    </div>
+  );
+}
